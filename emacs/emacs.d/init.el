cdd3a0dea4cdd2ab67ce64fd4a432b1fdf61a342
Nathan Barlow <nathanjbarlow@gmail.com>
2023-12-21 20:25:37 -0600

    Add customize vars to emacs config

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -93,3 +93,16 @@
   (define-key company-active-map (kbd "C-p") 'company-select-previous))
 
 (use-package flycheck :config (global-flycheck-mode +1))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(flycheck flx-ido ido-completing-read+ ido-vertical-mode auto-compile company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
