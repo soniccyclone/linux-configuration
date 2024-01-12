03c2562a7aca96e5631080c204f647c6b8776928
Nathan Barlow <nathanjbarlow@gmail.com>
2024-01-11 19:22:45 -0600

    Add golang

---
+++ golang/setup.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+set -eo pipefail
+
+cd $(dirname ${BASH_SOURCE[0]})
+
+echo "Installing golang"
+
+curl -OL https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
+
+sudo rm -rf /usr/local/go
+
+sudo tar -C /usr/local -xvf go1.16.7.linux-amd64.tar.gz
+
