e4dc704505624882ceea696658892b9261fca680
Nathan Barlow <nathanjbarlow@gmail.com>
2025-06-26 21:47:30 -0500

    Add info files for emacs install

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -7,7 +7,9 @@ cd $(dirname ${BASH_SOURCE[0]})
 echo "Installing emacs."
 if sudo -v; then
     sudo apt-get -qy install \
-        emacs
+         emacs \
+	 emacs-common \
+	 emacs-common-non-dfsg
 fi
 
 echo "Linking .emacs configuration."
