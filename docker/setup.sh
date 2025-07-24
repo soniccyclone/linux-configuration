cf5d3e4640236ae2e7da2a289f6dbb269be9e8e4
Nathan Barlow <nathanjbarlow@gmail.com>
2025-07-24 16:36:38 -0500

    Revert docker install back to ubuntu

--- docker/setup.sh
+++ docker/setup.sh
@@ -6,10 +6,26 @@ cd $(dirname ${BASH_SOURCE[0]})
 
 # Add Docker's official GPG key:
 if sudo -v; then
-    sudo dnf -qy install dnf-plugins-core
-    sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    sudo apt-get -qy update
+    sudo apt-get -qy install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get -qy update
+
     # Install docker engine
-     sudo dnf install -qy docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get -qy install \
+	 docker-ce \
+	 docker-ce-cli \
+	 containerd.io \
+	 docker-buildx-plugin \
+	 docker-compose-plugin
 
     # Create docker group and add user to it
     if [ ! $(getent group docker) ]; then
@@ -19,5 +35,4 @@ if sudo -v; then
     # You will need to log out and back in to use docker without sudo.
     # You may also need to restart the docker service or your whole machine
     # as well.
-    sudo systemctl enable docker
 fi
