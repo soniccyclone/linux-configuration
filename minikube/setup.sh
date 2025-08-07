24c3b9a1e92311ad6e144dcaa647fde86b56405c
Nathan Barlow <nathanjbarlow@gmail.com>
2025-08-07 13:58:30 -0500

    Switch minikube to apt

--- minikube/setup.sh
+++ minikube/setup.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 cd $(dirname ${BASH_SOURCE[0]})
 
 if sudo -v; then
-    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
-    sudo rpm -Uvh minikube-latest.x86_64.rpm
-    rm minikube-latest.x86_64.rpm
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+    sudo dpkg -i minikube_latest_amd64.deb
+    rm minikube_latest_amd64.deb
 fi
