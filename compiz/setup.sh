f9e7e65bd2b4a51ebf0452b4c17ecb3f69f6c160
Nathan Barlow <nathanjbarlow@gmail.com>
2025-10-11 11:06:28 -0500

    Delete things not need on kde

--- compiz/setup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -eo pipefail
-
-cd $(dirname ${BASH_SOURCE[0]})
-
-if sudo -v; then
-    sudo apt-get -y install compiz compiz-plugins-experimental emerald emerald-themes
-fi
--- screensavers/setup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -eo pipefail
-
-cd $(dirname ${BASH_SOURCE[0]})
-
-if sudo -v; then
-    sudo apt-get -y install xscreensaver xscreensaver-gl-extra xscreensaver-data-extra
-fi
