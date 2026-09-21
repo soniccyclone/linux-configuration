86400aae272fc94913578919fe1afcb101093e87
Nathan Barlow <nathanjbarlow@gmail.com>
2026-09-21 12:03:57 -0500

    Bunch more modernizing emacs

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -39,43 +39,22 @@
 ;; Stops emacs from forcing me to type "yes" instead of hitting "y"
 (setq use-short-answers t)
 
-;; Enable moving around windows instead of only cycling through them
-(global-set-key (kbd "<C-M-up>")    'windmove-up)
-(global-set-key (kbd "<C-M-down>")  'windmove-down)
-(global-set-key (kbd "<C-M-left>")  'windmove-left)
-(global-set-key (kbd "<C-M-right>") 'windmove-right)
+;; Shift and arrow keys moves around windows
+(windmove-default-keybindings)
 
-;; Setup line numbers
+;; line numbers
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
-;; Turn on 80 column marker
+;; 80 column marker line
 (add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
 
-;; Highlight cursor line
+;; cursor line highlight
 (global-hl-line-mode)
-
-;; Set up use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-and-compile
-  (setq use-package-always-ensure t))
 
 ;; enable column numbers
 (use-package simple
   :ensure nil
   :config (column-number-mode +1))
-
-(use-package eldoc
-  :ensure nil
-  :diminish eldoc-mode
-  :config
-  (setq eldoc-idle-delay 0.4))
-
-(use-package paren
-  :ensure nil
-  :init (setq show-paren-delay 0)
-  :config (show-paren-mode +1))
 
 ;; delete intermediate buffers when navigating dired
 (use-package dired
@@ -87,25 +66,9 @@
         (put 'dired-find-alternate-file 'disabled nil)
         (define-key dired-mode-map (kbd "RET") #'dired-find-alternate-file))))
 
-(use-package ido
-  :config
-  (ido-mode +1)
-  (setq ido-everywhere t
-        ido-enable-flex-matching t))
-
-(use-package ido-vertical-mode
-  :config
-  (ido-vertical-mode +1)
-  (setq ido-vertical-define-keys 'C-n-C-p-up-and-down))
-
-(use-package ido-completing-read+
-  :config (ido-ubiquitous-mode +1))
-
-(use-package flx-ido
-  :config (flx-ido-mode +1))
+(fido-vertical-mode)
 
 (use-package company
-  :diminish company-mode
   :hook (prog-mode . company-mode)
   :config
   (setq company-minimum-prefix-length 1
@@ -117,14 +80,8 @@
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous))
 
-(use-package flycheck
-  :config (global-flycheck-mode +1))
-
-(use-package go-mode)
-
 (use-package geiser)
 (use-package geiser-mit)
-(use-package paredit)
 (use-package macrostep)
 
 (use-package macrostep-geiser
@@ -177,7 +134,6 @@
  '(byte-compile-warnings '(not obsolete))
  '(display-fill-column-indicator-column 80)
  '(native-comp-async-report-warnings-errors 'silent)
- '(package-selected-packages nil)
  '(warning-supress-log-types '((comp) (bytecomp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
