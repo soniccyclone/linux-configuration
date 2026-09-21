4e2ee6bd484bf0d48ac2194a4737b558011db666
Nathan Barlow <nathanjbarlow@gmail.com>
2026-09-21 11:43:24 -0500

    cleanup some comments in emacs and remove java lsp mode

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -54,7 +54,7 @@
 ;; Highlight cursor line
 (global-hl-line-mode)
 
-;; Setting up the package manager. Install if missing.
+;; Set up use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -163,36 +163,10 @@
   (add-hook 'scheme-mode-hook           'enable-paredit-mode)
   (add-hook 'clojure-mode-hook               'enable-paredit-mode))
 
-;; lsp-java quick start
-(condition-case nil
-    (require 'use-package)
-  (file-error
-   (require 'package)
-   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-   (package-initialize)
-   (package-refresh-contents)
-   (package-install 'use-package)
-   (setq use-package-always-ensure t)
-   (require 'use-package)))
-(use-package projectile)
-(use-package flycheck)
-(use-package yasnippet :config (yas-global-mode))
-(use-package lsp-mode
-  :hook ((lsp-mode . lsp-enable-which-key-integration))
-  :init (setq lsp-keymap-prefix "s-k"))
-(use-package hydra)
-(use-package company)
-(use-package lsp-ui)
-(use-package which-key :config (which-key-mode))
-(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
-(use-package dap-mode :after lsp-mode :config (dap-auto-configure-mode))
-(use-package dap-java :ensure nil)
-(use-package lsp-treemacs)
-
-;; Always follow symlinks
+;; Always follow symlinks.
 ;; It's not dangerous to follow version controlled
 ;; symlinks by default now since git doesn't rely
-;; on lockfiles like ancient VC systems did
+;; on lockfiles like ancient VC systems did.
 (setq vc-follow-symlinks t)
 
 (custom-set-variables
